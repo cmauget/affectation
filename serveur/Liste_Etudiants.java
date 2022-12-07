e4e11766441b5b20cb63f07af0e48c78ffb41f74
valdomenech <valentin.domenech@insa-rouen.fr>
2022-12-07 15:35:08 +0100

    Classe Liste_Etudiants à tester

--- serveur/Liste_Etudiants.java
+++ serveur/Liste_Etudiants.java
@@ -1,8 +1,7 @@
-package affectation.serveur;
-
 import java.util.*;
+import java.io.Serializable;
 
-public class Liste_Etudiants{
+public class Liste_Etudiants implements Serializable{
     //Attribut
     public ArrayList<Etudiant> liste_Etudiants;
 
@@ -21,7 +20,13 @@ public class Liste_Etudiants{
 
     public ArrayList<Etudiant> getListe_Etudiants(){
         return liste_Etudiants;
-}
+    }
+    public void creerEtudiant(Etudiant e){
+        this.liste_Etudiants.add(e);
+    }
+    public boolean authEtudiant(Etudiant e){
+        return (liste_Etudiants.contains(e));
+    }
 }
 
 
