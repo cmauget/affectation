60d8c8e3151a9891fb138642e3faf977ec22dc1f
Manon Rose <manon.rose@insa-rouen.fr>
2022-12-07 16:15:39 +0100

    test de la classe Liste_Etudiants ok yesssssss

--- serveur/Liste_EtudiantsTest.java
+++ serveur/Liste_EtudiantsTest.java
@@ -1,12 +1,15 @@
-import java.util.*;
+
 
 public class Liste_EtudiantsTest{
     public static void main(String[] args) {
         Etudiant e = new Etudiant("facundo","aaa");
-        ArrayList<Etudiant> l1 = new ArrayList<Etudiant>();
+        Etudiant e2 = new Etudiant("manon","wsh");
+        Liste_Etudiants liste_Etudiants = new Liste_Etudiants();
         
-        l1.creerEtudiant(e);
-        System.out.println(l1);
-
+        liste_Etudiants.creerEtudiant(e);
+        liste_Etudiants.creerEtudiant(e2);
+        System.out.println(liste_Etudiants);
+        System.out.println("l etudiant " + e + liste_Etudiants.authEtudiant(e));
+        System.out.println(liste_Etudiants.authEtudiant(e2));
     }
 }
