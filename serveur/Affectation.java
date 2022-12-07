a543e8331cfb02fc3263c110cdeabe3979d2b924
Manon Rose <manon.rose@insa-rouen.fr>
2022-12-07 16:27:11 +0100

    classe affecation commencée

---
+++ serveur/Affectation.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Affectation {
+
+    //Attributs 
+    public Affectation(){
+        public ArrayList<Choix> liste_Choix_Traité;
+        public ArrayList<Choix> liste_Choix_Brut;
+    }
+    
+}
