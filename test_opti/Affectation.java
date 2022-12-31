47e1bddd6b8b0a655eba408a092e5184e117ad4a
Clement Mauget <clement.mauget@gmail.com>
2022-12-31 13:30:55 +0100

    oskour

---
+++ test_opti/Affectation.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Affectation {
+
+    //Attributs
+    public ArrayList<Choix> liste_Choix_Traite;
+    public ArrayList<Choix> liste_Choix_Brut;
+
+    //Méthodes
+    public Affectation(){
+        this.liste_Choix_Traite = new ArrayList<Choix>();
+        this.liste_Choix_Brut=new ArrayList<Choix>();
+    }
+    public Affectation(ArrayList<Choix> liste_Choix_Traite, ArrayList<Choix> liste_Choix_Brut){
+        this.liste_Choix_Traite = liste_Choix_Traite;
+        this.liste_Choix_Brut = liste_Choix_Brut;
+    }
+
+    public void setListeChoixTraite(ArrayList<Choix> liste_Choix_Traite){
+        this.liste_Choix_Traite=liste_Choix_Traite;
+    }
+
+    public void setListeChoixBrut(ArrayList<Choix> liste_Choix_Brut){
+        this.liste_Choix_Brut=liste_Choix_Brut;
+    }
+
+
+    public ArrayList<Choix> getListeChoixTraite(){
+        return liste_Choix_Traite;
+    }
+    public ArrayList<Choix> getListe_Choix_Brut(){
+        return liste_Choix_Brut;
+    }
+
+    public void optimiserAffectations(){
+
+        //TEMPORAIRE
+        liste_Choix_Traite=liste_Choix_Brut;
+
+    }
+
+    public void ajouterChoix(Choix c){
+                this.liste_Choix_Brut.add(c);
+    }
+
+
+
+    public Choix retournerAffectation(Etudiant e){
+        Choix target = new Choix();
+        target.setId(e);
+        int index = liste_Choix_Traite.indexOf(target);
+        Choix choix = liste_Choix_Traite.get(index);
+        return choix;
+    }
+
+    public boolean moduleOuvert(String module){
+        return false;
+
+
+    }
+}
---
+++ test_opti/Choix.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.Serializable;
+
+public class Choix implements Serializable{
+
+    public Etudiant id;
+    public ArrayList<String> liste_choix_pref;
+    public ArrayList<String> liste_choix_sec;
+
+    public Choix(){
+        this.id=new Etudiant();
+        this.liste_choix_pref=new ArrayList<String>();
+        this.liste_choix_sec=new ArrayList<String>();
+    }
+
+    public Choix(Etudiant id, ArrayList<String> liste_choix_pref, ArrayList<String> liste_choix_sec){
+        this.id=id;
+        this.liste_choix_pref=liste_choix_pref;
+        this.liste_choix_sec=liste_choix_sec;
+    }
+
+    public void setId(Etudiant id){
+        this.id=id;
+    }
+    public void setListe_choix_pref(ArrayList<String> liste_choix_pref){
+        this.liste_choix_pref=liste_choix_pref;
+    }
+    public void setListe_choix_sec(ArrayList<String> liste_choix_sec){
+        this.liste_choix_sec=liste_choix_sec;
+    }
+    public Etudiant getId(){
+        return id;
+    }
+    public ArrayList<String> getListe_choix_pref(){
+        return liste_choix_pref;
+    }
+    public ArrayList<String> getListe_choix_sec(){
+        return liste_choix_sec;
+    }
+
+
+    //Méthode générique pour vérifier les doublons dans un ArrayList
+
+    public boolean pasDeDoublon(ArrayList<String> l){
+        // pour chaque élément du ArrayList, vérifie s'il se retrouve ensuite dans le ArrayList
+        for (int i = 0; i < l.size(); i++)
+        {
+            for (int j = i + 1; j < l.size(); j++)
+            {
+                if (l.get(i) != null && l.get(i).equals(l.get(j))) {
+                    return false;
+                }
+            }
+        }
+
+        // aucun doublon n'est trouvé
+        return true;
+    }
+
+    public void setChoix_pref(String matiere, int place, ArrayList<String> liste_choix_pref){
+        liste_choix_pref.add(place,matiere);
+    }
+
+    public void setChoix_sec(String matiere, int place, ArrayList<String> liste_choix_sec){
+        liste_choix_sec.add(place,matiere);
+    }
+
+    public boolean choixValide(){
+        return ((this.liste_choix_pref.size()==8))&&((this.liste_choix_sec.size()==4));
+    }
+
+    //Redefinition equals
+    public boolean equals(Object o){
+      if (this==o) return true;
+      if (o == null || getClass()!= o.getClass()) return false;
+      Choix choix = (Choix) o;
+      return id == choix.id;
+    }
+
+
+}
