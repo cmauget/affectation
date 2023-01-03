5bcd88f2733539b5bfecceede68ae9ee3ed5855b
valdomenech <valentin.domenech@insa-rouen.fr>
2023-01-03 17:51:34 +0100

    finito pipo

--- serveur/Accepter_clients.java
+++ serveur/Accepter_clients.java
@@ -1,13 +1,8 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
-import java.util.concurrent.*;
 
 public class Accepter_clients implements Runnable {
 
